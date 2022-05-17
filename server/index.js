0e15fb585f617c2c84c0d8665442808251b698b3
neilson <joseneilson25@gmail.com>
2022-05-17 04:57:47 -0300

    delete

--- server/index.js
+++ server/index.js
@@ -48,6 +48,18 @@ app.put("/edit", (req, res) =>{
     })
 })
 
+app.delete("/delete/:id", (req, res) => {
+    const { id } = req.params;
+    let SQL = "DELETE FROM games WHERE idgames = ?";
+    db.query(SQL, [id], (err, result) => {
+      if (err) {
+        console.log(err);
+      } else {
+        res.send(result);
+      }
+    });
+  });
+
 app.listen(3001, ()=>{
     console.log("rodando servidor");
 });
