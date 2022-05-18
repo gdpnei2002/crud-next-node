49949cd38c102513e5ecbf22e71164579c33df0f
neilson <joseneilson25@gmail.com>
2022-05-17 23:46:43 -0300

    paginação

---
+++ client/pages/list.js
@@ -0,0 +1,50 @@
+import React, {useState, useEffect } from 'react';
+import Axios from "axios"
+import Card from '../components/cards/card';
+
+export default function Home() {
+
+  const [values, setValues] = useState();
+  const [listGames, setListGames] = useState();
+
+  const handleChangeValues = (value) =>{
+    setValues(prevValue=>({
+      ...prevValue,
+      [value.target.name]: value.target.value,
+    }));
+};
+
+const handleClickButton = () =>{
+  Axios.post("http://localhost:3001/register", {
+    name: values.name,
+    cost: values.cost,
+    category: values.category,
+}).then((response) => {
+  console.log(response);
+})}
+
+useEffect(() =>{
+  Axios.get("http://localhost:3001/getCards").then((response) =>{
+    setListGames(response.data);
+  })
+},[])
+
+  return (
+    <div className="app-container">
+      {typeof listGames !=="undefined" &&
+          listGames.map((value) => {
+            return(
+              <Card
+                key={value.id}
+                listCard={listGames}
+                setListGames={setListGames}
+                id={value.idgames}
+                name={value.name}
+                cost={value.cost}
+                category={value.category}>
+              </Card>
+            )
+          })}
+  </div>
+  )
+}
---
+++ client/pages/register.js
@@ -0,0 +1,63 @@
+import React, {useState, useEffect } from 'react';
+import Axios from "axios"
+
+export default function Home() {
+
+  const [values, setValues] = useState();
+  const [listGames, setListGames] = useState();
+
+  const handleChangeValues = (value) =>{
+    setValues(prevValue=>({
+      ...prevValue,
+      [value.target.name]: value.target.value,
+    }));
+};
+
+const handleClickButton = () =>{
+  Axios.post("http://localhost:3001/register", {
+    name: values.name,
+    cost: values.cost,
+    category: values.category,
+}).then((response) => {
+  console.log(response);
+})}
+
+useEffect(() =>{
+  Axios.get("http://localhost:3001/getCards").then((response) =>{
+    setListGames(response.data);
+  })
+},[])
+
+  return (
+    <div className="app-container">
+    <div className="register-container">
+      <h1 className="register-title">My Cardinho</h1>
+        <input 
+          type="text" 
+          name="name" 
+          placeholder=" Nome" 
+          className="register-input"
+          onChange={handleChangeValues}/>
+      <input 
+        type="text" 
+        name="cost" 
+        placeholder=" Preço" 
+        className="register-input"
+        onChange={handleChangeValues}/>
+      <input 
+        type="text" 
+        name="category" 
+        placeholder=" Categoria" 
+        className="register-input"
+        onChange={handleChangeValues}/>
+    <a href='/list'>
+      <button 
+        className="register-button" 
+        onClick={()=>handleClickButton()}>
+          Cadastrar
+      </button>
+    </a>
+    </div>
+  </div>
+  )
+}
